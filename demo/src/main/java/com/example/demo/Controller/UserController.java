afcf6d867b0a366c9a3db7b95d057d03f49b2257
Singh <Vaibhav.Singh2@Honeywell.com>
2019-08-30 12:33:43 +0530

    Added files

---
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,94 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Entity.User;
+import com.example.demo.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ExpressionException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    /**
+     * Get all users list.
+     *
+     * @return the list
+     */
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    /**
+     * Gets users by id.
+     *
+     * @param userId the user id
+     * @return the users by id
+     */
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUsersById(@PathVariable(value = "id") Long userId) {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new RuntimeException("User not found on :: " + userId));
+        return ResponseEntity.ok().body(user);
+    }
+    /**
+     * Create user user.
+     *
+     * @param user the user
+     * @return the user
+     */
+    @PostMapping("/users")
+    public User createUser(@Valid @RequestBody User user) {
+        return userRepository.save(user);
+    }
+    /**
+     * Update user response entity.
+     *
+     * @param userId the user id
+     * @param userDetails the user details
+     * @return the response entity
+     */
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetails)
+            throws RuntimeException {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new RuntimeException("User not found on :: " + userId));
+        user.setEmail(userDetails.getEmail());
+        user.setLastName(userDetails.getLastName());
+        user.setFirstName(userDetails.getFirstName());
+        user.setUpdatedAt(new Date());
+        final User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+    /**
+     * Delete user map.
+     *
+     * @param userId the user id
+     * @return the map
+     * @throws Exception the exception
+     */
+    @DeleteMapping("/user/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws Exception {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new RuntimeException("User not found on :: " + userId));
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+}
